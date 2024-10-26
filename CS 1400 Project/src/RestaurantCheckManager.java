7af1c9aa34e80b0fa12051531878e2724d5094a0
Noah Arnold <111319826+FormalSpecialist@users.noreply.github.com>
2024-10-25 18:15:08 -0700

    Start to the Project
    The .java file has the correct class and set up to allow for the initial methods to be created. They are currently hard coded so they can only occur for a single test case.

---
+++ CS 1400 Project/src/RestaurantCheckManager.java
@@ -0,0 +1,33 @@
+public class RestaurantCheckManager {
+    public static void main(String[] args) throws Exception {
+        
+        // Create Variables
+        double saleAmount = 0.0, tipAmount = 0.0, totalAmount = 0.0;
+        double totalSaleAmount = 0.0, totalTipAmount = 0.0;
+        int numChecks = 0;
+
+
+        // Start of Loop
+        saleAmount = 15.34;
+        tipAmount = 4.66;
+        totalAmount = 20.00;
+
+        totalSaleAmount = saleAmount;
+        totalTipAmount = tipAmount;
+        numChecks++;
+        
+        // Print
+        
+
+
+        
+        // Call Method for Breakdowns of Tips
+
+
+
+        // Call Method for Individual Breakdown
+    
+    
+    
+    }
+}
